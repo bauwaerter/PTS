977424fee3c7028cd66879c2a7fda2a00e4645c7
incognito89 <brandon.auwaerter89@gmail.com>
2013-11-17 21:13:12 -0800

    try two on committing crap

--- PTS/Controllers/SearchController.cs
+++ PTS/Controllers/SearchController.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Domains;
+using Service.Interfaces;
+using PTS.Infrastructure;
+using PTS.Models;
 
 namespace PTS.Views.Search
 {
     public class SearchController : Controller
     {
+        private readonly IBaseService<TeacherUser> _teacherUserService;
+
+        public SearchController(IBaseService<TeacherUser> teacherUserService)
+        {
+            _teacherUserService = teacherUserService;
+        }
+
         //
         // GET: /Search/
-
+        
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult GetTeacherUsers()
+        {
+            var data = _teacherUserService.GetAll().ToList();
+
+            return Json(new { Results = "OK", Records = data });
+        }
     }
 }
