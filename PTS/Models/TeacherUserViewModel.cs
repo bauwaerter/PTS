b07fc8b6957acf0e783ff6296d4e8602e0d6ab37
incognito89 <brandon.auwaerter89@gmail.com>
2013-11-19 12:05:29 -0800

    added teacher user model

---
+++ PTS/Models/TeacherUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Core.Domains;
+
+namespace PTS.Models
+{
+    public class TeacherUserViewModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal HourlyRate { get; set; }
+        public decimal ClassRate { get; set; }
+
+        public TeacherUserViewModel()
+        {
+
+        }
+
+    }
+}
