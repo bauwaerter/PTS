5a3aedb55bfa6c62d9b89d9adcf130afbcd66b25
incognito89 <brandon.auwaerter89@gmail.com>
2013-11-19 19:18:37 -0800

    added a few things to menu bar and jtables

--- PTS/Controllers/SearchController.cs
+++ PTS/Controllers/SearchController.cs
@@ -14,11 +14,14 @@ namespace PTS.Views.Search
     {
         private readonly IBaseService<TeacherUser> _teacherUserService;
         private readonly IUserService _userService;
+        private readonly IBaseService<Class> _classService;
 
-        public SearchController(IBaseService<TeacherUser> teacherUserService, IUserService userService)
+        public SearchController(IBaseService<TeacherUser> teacherUserService, IUserService userService,
+                                IBaseService<Class> classService)
         {
             _teacherUserService = teacherUserService;
             _userService = userService;
+            _classService = classService;
         }
 
         //
@@ -42,10 +45,10 @@ namespace PTS.Views.Search
                     FirstName = s.FirstName,
                     LastName = s.LastName,
                     Email = s.Email,
-                    
+
                 }).ToList();
 
-               
+                
 
                 return Json(new { Result = "OK", Records = data });
             }
@@ -61,6 +64,20 @@ namespace PTS.Views.Search
                 _teacherUserService.Insert(teacherUser);
                 
                 var data = teacherUser;
+
+                return Json(new { Results = "OK", Record = data });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult GetClasses()
+        {
+            try
+            {
+                var data = _classService.GetAll().ToList();
 
                 return Json(new { Results = "OK", Record = data });
             }
