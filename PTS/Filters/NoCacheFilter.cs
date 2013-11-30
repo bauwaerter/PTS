08f0b53e7da17e866924fbbafb1a505a6b77f4cf
streylight <straylightthegreat@gmail.com>
2013-11-30 14:39:18 -0800

    cache filters

---
+++ PTS/Filters/NoCacheFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using System;
+using System.Web;
+
+namespace Core.PTS.Filters {
+    /// <summary>
+    /// NoCache Filter
+    /// </summary>
+    public class NoCacheFilter : ActionFilterAttribute {
+        /// <summary>
+        /// Called by the ASP.NET MVC framework before the action result executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public override void OnResultExecuting(ResultExecutingContext filterContext) {
+            filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            filterContext.HttpContext.Response.Cache.SetValidUntilExpires(false);
+            filterContext.HttpContext.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            filterContext.HttpContext.Response.Cache.SetNoStore();
+            base.OnResultExecuting(filterContext);
+        }
+    }
+}
+
