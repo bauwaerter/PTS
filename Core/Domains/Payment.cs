13896a397cc5e59b41a6ee6cfe5219b397dc5789
incognito89 <brandon.auwaerter89@gmail.com>
2013-11-30 14:37:41 -0800

    added payment

---
+++ Core/Domains/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace Core.Domains
+{
+    public class Payment : BaseEntity
+    {
+        public virtual int StudentId { get; set; }
+        public virtual int TeacherId { get; set; }
+        public virtual int? ClassId { get; set; }
+        public virtual double Money { get; set; }
+        public virtual string Description { get; set; }
+        public virtual int CreditCardNumber { get; set; }
+        public virtual DateTime Date { get; set; }
+    }
+}
---
+++ Data/Mappings/PaymentMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Core.Domains;
+
+namespace Data.Mappings
+{
+    public class PaymentMap : EntityTypeConfiguration<Payment>
+    {
+        public PaymentMap()
+        {
+            ToTable("Payment", "dbo");
+            HasKey(p => p.Id);
+                
+        }
+    }
+}
