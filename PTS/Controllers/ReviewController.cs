a90c20b709a92aa38a19fd54b685c9b6c64b3499
seangibeault <seangibeault@gmail.com>
2013-11-30 17:24:35 -0800

    added review class

--- PTS/Controllers/ReviewController.cs
+++ PTS/Controllers/ReviewController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Domains;
+using PTS.Models;
 using Service.Interfaces;
 using Service.Services;
 using System;
@@ -6,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace PTS.Views.Review
 {
     public class ReviewController : Controller
@@ -23,25 +23,124 @@ namespace PTS.Views.Review
         private readonly IBaseService<TeacherUser> _teacherUserService;
         private readonly IBaseService<Class> _classService;
         private readonly IBaseService<Location> _locationService;
+        private readonly IBaseService<ReviewClass> _reviewClassService;
+
 
         #endregion
         //
         // GET: /Review/
 
-        public ReviewController(IBaseService<Enrolled> enrolledService, IUserService userService, IBaseService<StudentUser> studentUserService, 
+        public ReviewController(IBaseService<ReviewClass> reviewClass,IBaseService<Enrolled> enrolledService, IUserService userService, IBaseService<StudentUser> studentUserService, 
                                 IBaseService<Class> classService, IBaseService<Location> locationService, IBaseService<TeacherUser> teacherUserService)
         {
             _enrolledService = enrolledService;
-            
+            _classService = classService;
+            _reviewClassService = reviewClass;
         }
 
         public ActionResult Index()
         {
             
-            //var ClassesTaken = _enrolledService.GetTableQuery().Where(e => e.StudentId == 15).ToList();
-
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AddUpdateClassesToReview(ReviewClassViewModel review)
+        {
+            try
+            {
+                var ReviewClass = _reviewClassService.GetTableQuery().Where(e => e.StudentId == review.StudentID).ToList();
+                var classdesc = _classService.GetById(review.ClassID);
+                var record = new ReviewClassViewModel();
+                if(ReviewClass.Count()>0)
+                {
+                    var update = new ReviewClass
+                    {
+                        Id=ReviewClass.SingleOrDefault().Id,
+                        ClassId = review.ClassID,
+                        Comment = review.Comment,
+                        Date = DateTime.Today,
+                        StudentId = review.StudentID,
+                        Rating = review.Rating,
+                    };
+                    _reviewClassService.Update(update);
+
+                    record = new ReviewClassViewModel
+                    {
+                        ClassID = review.ClassID,
+                        Comment = review.Comment,
+                        Date = DateTime.Today,
+                        StudentID = review.StudentID,
+                        Rating = review.Rating,
+                        Description = classdesc.Description,
+                    };
+                }
+                else
+                {
+                    var insert = new ReviewClass();
+                    insert.ClassId = review.ClassID;
+                    insert.Comment = classdesc.Description;
+                    insert.Date = DateTime.Today;
+                    insert.StudentId = review.StudentID;
+                    insert.Rating = review.Rating;
+                    _reviewClassService.Insert(insert);
+
+                    record = new ReviewClassViewModel
+                    {
+                        ClassID = review.ClassID,
+                        Comment = review.Comment,
+                        Date = DateTime.Today,
+                        StudentID = review.StudentID,
+                        Rating = review.Rating,
+                        Description = classdesc.Description,
+                    };
+                }
+
+
+                
+                return Json(new { Result = "OK", Record = record });
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public ActionResult GetClassesToReview()
+        {
+            var EnrolledClasses = _enrolledService.GetTableQuery().Where(e => e.StudentId == 15).ToList();
+            var Class = new List<Class>();
+            var Reviews = new List<ReviewClassViewModel>();
+            
+            foreach(var c in EnrolledClasses)
+            {
+                var temp =new ReviewClassViewModel();
+                var tempClassReview = _reviewClassService.GetTableQuery().Where(r => r.ClassId == c.ClassId).SingleOrDefault();
+                var tempClass = _classService.GetById(c.ClassId);
+                temp.Description = tempClass.Description;
+                temp.ClassID = tempClass.Id;
+                temp.StudentID = 15;
+                if (tempClassReview != null)
+                {
+                    temp.Comment = tempClassReview.Comment;
+                    temp.Date = tempClassReview.Date;
+                    temp.Rating = tempClassReview.Rating;
+                }
+                else
+                {
+                    temp.Comment = "Review this Class!";
+                    temp.Date = DateTime.Today.Date;
+                    temp.Rating = 0;
+                }
+
+                Reviews.Add( temp);
+            }
+
+            
+
+
+            var records = Reviews;
+            return Json(new { Result = "OK", Records = records });
+        }
     }
 }
---
+++ PTS/Models/ReviewClassViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTS.Models
+{
+    public class ReviewClassViewModel
+    {
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+        public int ClassID { get; set; }
+        public int StudentID { get; set; }
+
+        public ReviewClassViewModel()
+        {
+
+        }
+
+
+    }
+}
